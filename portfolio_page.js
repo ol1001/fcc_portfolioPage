9bc8ea17d5cd0a6b1b35b2c3c515085ec3546db9
Olha Stratii <shmelova83@gmail.com>
2016-05-08 21:18:05 +0300

    Initial commit working code

---
+++ portfolio_page.js
@@ -0,0 +1,22 @@
+
+
+var modal = document.getElementById('myModal');
+var img = document.getElementsByClassName("img-item");
+
+var modalImg = document.getElementById("img01");
+
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
